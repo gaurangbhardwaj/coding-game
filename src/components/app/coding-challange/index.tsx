49114844d15cb03d2f3005f21b321a6fbab272d0
Gaurang Bhardwaj <gaurang.bhardwaj@sleek.com>
2024-01-20 19:19:28 +0530

    No test fix

--- src/components/app/coding-challange/index.tsx
+++ src/components/app/coding-challange/index.tsx
@@ -1,4 +1,4 @@
-import React, { memo } from "react";
+import React, { memo, useEffect } from "react";
 
 import QuestionText from "./components/question";
 import ExampleList from "./components/example-list";
@@ -7,7 +7,18 @@ import EditorConsole from "./components/editor-console";
 import "./codingChallange.scss";
 import Header from "./components/header";
 
+import { useDispatch, useSelector } from "react-redux";
+import { fetchChallenges, selectQuestionBank } from "src/redux/slices";
+
 const CodingChallange: React.FC = () => {
+  const dispatch = useDispatch<any>();
+  const questionBank = useSelector(selectQuestionBank);
+
+  useEffect(() => {
+    if (!questionBank?.length) dispatch(fetchChallenges());
+  }, [dispatch, questionBank?.length]);
+
+  if (!questionBank?.length) return <div>No Test Found</div>;
   return (
     <div className="coding-challange-container">
       <Header />
